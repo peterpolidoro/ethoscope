44014987284d373275c5a654533ac6be268717dd
selmalaki <selmalaki@ucla.edu>
2019-01-31 16:44:31 -0500

    add the new classes (janelia sleep dep stimulator and roi builder) to the list of control

--- src/ethoscope/web_utils/control_thread.py
+++ src/ethoscope/web_utils/control_thread.py
@@ -24,7 +24,7 @@ from ethoscope.stimulators.stimulators import DefaultStimulator
 
 
 
-from ethoscope.stimulators.sleep_depriver_stimulators import SleepDepStimulator, OptomotorSleepDepriver, ExperimentalSleepDepStimulator, MiddleCrossingStimulator#, SystematicSleepDepInteractor
+from ethoscope.stimulators.sleep_depriver_stimulators import SleepDepStimulator, JaneliaSleepDepStimultor, OptomotorSleepDepriver, ExperimentalSleepDepStimulator, MiddleCrossingStimulator#, SystematicSleepDepInteractor
 from ethoscope.stimulators.odour_stimulators import DynamicOdourSleepDepriver, MiddleCrossingOdourStimulator #, DynamicOdourDeliverer
 from ethoscope.stimulators.optomotor_stimulators import OptoMidlineCrossStimulator
 
@@ -78,6 +78,7 @@ class ControlThread(Thread):
         "interactor":{
                         "possible_classes":[DefaultStimulator, 
                                             SleepDepStimulator,
+                                            JaneliaSleepDepStimultor,
                                             OptomotorSleepDepriver,
                                             MiddleCrossingStimulator,
                                             #SystematicSleepDepInteractor,
