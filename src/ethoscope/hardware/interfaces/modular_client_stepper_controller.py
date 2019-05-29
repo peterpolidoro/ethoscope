9a86864ff103cb6d74093eb2ae670313d0d5f8c4
selmalaki <selmalaki@ucla.edu>
2019-05-29 11:51:09 -0400

    add shaking stimulator method and limit the settings of the modular clients

--- src/ethoscope/hardware/interfaces/modular_client_stepper_controller.py
+++ src/ethoscope/hardware/interfaces/modular_client_stepper_controller.py
@@ -48,15 +48,15 @@ class ModularClientInterface(BaseInterface):
         if self._dev0 is not None and self._dev1 is not None:
             self._dev0.stop_all()
             self._dev0.zero_all()
-            self._dev0.acceleration_max('setAllElementValues', '1000')  # fix the acceleration of all the motors at 1000
-            self._dev0.velocity_max('setAllElementValues', '2520')
-            self._dev0.velocity_min('setAllElementValues', '-2520')
+            self._dev0.acceleration_max('setAllElementValues', '10000')  # fix the acceleration of all the motors at 1000
+            self._dev0.velocity_max('setAllElementValues', '720')
+            self._dev0.velocity_min('setAllElementValues', '-720')
             
             self._dev1.stop_all()
             self._dev1.zero_all()
-            self._dev1.acceleration_max('setAllElementValues', '1000')  # fix the acceleration of all the motors at 1000
-            self._dev1.velocity_max('setAllElementValues', '2520')
-            self._dev1.velocity_min('setAllElementValues', '-2520')
+            self._dev1.acceleration_max('setAllElementValues', '10000')  # fix the acceleration of all the motors at 1000
+            self._dev1.velocity_max('setAllElementValues', '720')
+            self._dev1.velocity_min('setAllElementValues', '-720')
             
         else:
             raise Exception("Could not initialize the motor boards.")
@@ -110,7 +110,56 @@ class ModularClientInterface(BaseInterface):
 
         return
     
-    
+
+    def shake_with_speed(self, board, channel, speed=180, acceleration=100000, deceleration=100000, duration=5000):
+        """
+        Move a specified rotation to a speed for a certain time.
+
+        :param board: the board number (0 or 1)
+        :type channel: int
+        :param channel: the number of the stepper motor to be moved
+        :type channel: int
+        :param speed: the speed, between -360 and 360 (degree/s). The sign indicates the rotation direction (CW or CCW)
+        :type speed: int
+        :param acceleration: the acceleration, between 100 and 10000 (degree/s^2).
+        :type acceleration: int
+        :param deceleration the deceleration, between 100 and 10000 (degree/s^2).
+        :type deceleration: int
+        :param duration: the time (ms) the stimulus should last
+        :type duration: int
+        :return:
+        """
+
+        if channel < 0 or channel > 6:
+            raise Exception("idx channel must be between 0 and 6")
+
+        if board < 0 or board > 1:
+            raise Exception("idx board must be 0 or 1")
+
+
+        motor = None
+
+        if board == 0:
+            motor = self._dev0
+        elif board == 1:
+            motor = self._dev1
+
+        if motor is not None:
+            for i in range(5):
+                # Change the rotation direction every iteration
+                if i%2 == 0: direction_speed = speed
+                else: direction_speed = -1 * speed
+
+                motor.move_at_for(channel, direction_speed, duration/5, acceleration, deceleration)
+                time.sleep(2)
+
+        else:
+            raise Exception("Motors are not configured to apply stimulus")
+
+        return
+
+
+
     def sleep_all(self):
         if self._dev0 is not None and self._dev1 is not None:
             self._dev0.sleep_all()
