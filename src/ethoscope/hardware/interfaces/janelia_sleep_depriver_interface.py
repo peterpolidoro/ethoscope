1581d8b21c5a0702bb4e4b43aeea2b9362dea23a
selmalaki <selmalaki@ucla.edu>
2019-05-29 11:49:30 -0400

    Shaker stimulator interface, change the name of the original interface to be adaptive stimulator interface

--- src/ethoscope/hardware/interfaces/janelia_sleep_depriver_interface.py
+++ src/ethoscope/hardware/interfaces/janelia_sleep_depriver_interface.py
@@ -3,7 +3,7 @@ __author__ = 'elmalakis'
 from ethoscope.hardware.interfaces.modular_client_stepper_controller import ModularClientInterface
 
 
-class JaneliaSleepDepriverInterface(ModularClientInterface):
+class JaneliaAdaptiveSleepDepriverInterface(ModularClientInterface):
     def send(self, board=None, channel=None, speed=180, duration=5000, acceleration=100, deceleration=100, velocity=0.5):
         """
         Sleep deprive an animal by rotating its tube.
@@ -29,5 +29,33 @@ class JaneliaSleepDepriverInterface(ModularClientInterface):
                              speed=speed,
                              duration=duration,
                              acceleration=acceleration,
-                             deceleration=deceleration
-                             )
+                             deceleration=deceleration)
+
+
+class JaneliaShakerSleepDepriverInterface(ModularClientInterface):
+    def send(self, board=None, channel=None, speed=180, duration=5000, acceleration=10000, deceleration=10000, velocity=0.5):
+        """
+        Sleep deprive an animal by shaking its tube.
+
+        :param board: the board number (0 or 1)
+        :type channel: int
+        :param channel: the number of the stepper motor to be moved
+        :type channel: int
+        :param speed: the speed, between -360 and 360 (degree/s) The sign indicates the rotation direction (CW or CCW)
+        :type speed: int
+        :param duration: the duration of time to rotate the tube in ms (max is 6s. The masking duration in ethomics is 6s)
+        :type: duration: int
+        :param acceleration: the acceleration, between 100 and 10000 (degree/sec^2)
+        :type: acceleration: int
+        :param deceleration: the deceleration, between 100 and 10000 (degree/sec^2)
+        :type: deceleration: int
+        :param velocity: fly velocity in m/s
+        :type: duration: float
+        """
+
+        self.shake_with_speed(board,
+                              channel,
+                              speed=speed,
+                              duration=duration,
+                              acceleration=acceleration,
+                              deceleration=deceleration)
