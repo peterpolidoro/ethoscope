43b0096df9237077e12cb25fa109072d0eb02c76
selmalaki <selmalaki@ucla.edu>
2019-06-06 12:05:53 -0400

    New interface for double stimulators

--- src/ethoscope/stimulators/stimulators.py
+++ src/ethoscope/stimulators/stimulators.py
@@ -21,7 +21,7 @@ class BaseStimulator(DescribedObject):
     _tracker = None
     _HardwareInterfaceClass = None
 
-    def __init__(self, hardware_connection, date_range=""):
+    def __init__(self, hardware_connection, date_range=""):  #Janelia adds another date range for the second date_range
         """
         Template class to interact with the tracked animal in a real-time feedback loop.
         Derived classes must have an attribute ``_hardwareInterfaceClass`` defining the class of the
@@ -32,7 +32,6 @@ class BaseStimulator(DescribedObject):
         :type hardware_connection: :class:`~ethoscope.hardware.interfaces.interfaces.BaseInterface`
         :param date_range: the start and stop date/time for the stimulator. Format described `here <https://github.com/gilestrolab/ethoscope/blob/master/user_manual/schedulers.md>`_
         :type date_range: str
-        
         """
 
         self._scheduler = Scheduler(date_range)
@@ -75,6 +74,92 @@ class BaseStimulator(DescribedObject):
     def _deliver(self, **kwargs):
         if self._hardware_connection is not None:
             self._hardware_connection.send_instruction(kwargs)
+
+
+
+class DoubleBaseStimulator(DescribedObject):
+    _tracker = None
+    _HardwareInterfaceClass = None
+
+    def __init__(self, hardware_connection, date_range="", date_range2=""):
+        """
+        Template class to interact with the tracked animal in a real-time feedback loop with two stimulus.
+        The first stimulus is a controlled stimulus from the RPi and the second stimulus is an external stimulus that
+        has its own protocol but only needs an ON and OFF signal
+
+        Derived classes must have an attribute ``_hardwareInterfaceClass`` defining the class of the
+        :class:`~ethoscope.hardware.interfaces.interfaces.BaseInterface` object (not on object) that instances will
+        share with one another. In addition, they must implement a ``_decide()`` method.
+
+        :param hardware_connection: The hardware interface to use.
+        :type hardware_connection: :class:`~ethoscope.hardware.interfaces.interfaces.BaseInterface`
+        :param date_range: the start and stop date/time for the stimulator. Format described `here <https://github.com/gilestrolab/ethoscope/blob/master/user_manual/schedulers.md>`_
+        :type date_range: str
+        :param date_range2: the start and stop date/time for the second stimulator. Format described `here <https://github.com/gilestrolab/ethoscope/blob/master/user_manual/schedulers.md>`_
+        :type date_range: str
+        """
+
+        self._scheduler = Scheduler(date_range)
+        self._scheduler2 = Scheduler(date_range2)
+        self._hardware_connection = hardware_connection
+        self._communicationflag=False
+
+
+    def apply(self):
+        """
+        Apply this stimulator. This method will:
+
+        1. check ``_tracker`` exists
+        2. decide (``_decide``) whether to interact
+        3. if 2. pass the interaction arguments to the hardware interface
+
+        :return: whether a stimulator has worked, and a result dictionary
+        """
+        if self._tracker is None:
+            raise ValueError("No tracker bound to this stimulator. Use `bind_tracker()` methods")
+
+        # check the second time range for the second stimulus
+        if self._scheduler2.check_time_range() is True and self._communicationflag is False:
+            self._communicate(True) # send an On Signal
+            self._communicationflag = True
+        elif self._scheduler2.check_time_range() is False and self._communicationflag is True:
+            self._communicate(False)  # send an OFF Signal
+            self._communicationflag = False
+
+        if self._scheduler.check_time_range() is False:
+            return HasInteractedVariable(False), {}
+        interact, result = self._decide()
+        if interact > 0:
+            self._deliver(**result)
+
+        return interact, result
+
+    def bind_tracker(self, tracker):
+        """
+        Link a tracker to this interactor
+
+        :param tracker: a tracker object.
+        :type tracker: :class:`~ethoscope.trackers.trackers.BaseTracker`
+        """
+        self._tracker = tracker
+
+    def _decide(self):
+        raise NotImplementedError
+
+
+    def _deliver(self, **kwargs):
+        if self._hardware_connection is not None:
+            self._hardware_connection.send_instruction(kwargs)
+
+
+    def _communicate(self, communicate_signal):
+        """
+        communicate an operating signal to an outside stimulator
+
+        :param communicate_signal: an ON and OFF signal to the external stimulator
+        :type communicate_signal: bool
+        """
+        raise NotImplementedError
 
 
 class DefaultStimulator(BaseStimulator):
