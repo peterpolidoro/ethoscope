df8e9f2b0d265029533c7ce69d2ca90479c2573b
selmalaki <selmalaki@ucla.edu>
2019-06-25 16:20:02 -0400

    debug

--- src/ethoscope/core/tracking_unit.py
+++ src/ethoscope/core/tracking_unit.py
@@ -95,17 +95,17 @@ class TrackingUnit(object):
 
         interact, result = self._stimulator.apply()
         #debug
-        # if len(result) > 0:
-        #     print('before len data rows')
-        #     print result
+        if len(result) > 0:
+            print('before len data rows')
+            print result
 
         if len(data_rows) == 0:
             return []
 
         #debug
-        # if any(result):
-        #     print('after len data rows')
-        #     print result
+        if any(result):
+            print('after len data rows')
+            print result
 
         # TODO data_row should have some result
         for dr in data_rows:
