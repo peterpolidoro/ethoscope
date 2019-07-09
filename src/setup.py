01ef4c429d01d80540bd22b8ad8ee1d329506336
selmalaki <selmalaki@ucla.edu>
2019-07-09 16:01:05 -0400

    fix setup requirements

--- src/setup.py
+++ src/setup.py
@@ -22,10 +22,10 @@ setup(
     # data e.g. classifiers can be added as part of the package
     # TODO
     # package_data={'ethoscope': ['data/classifiers/*.pkl']},
-    # Janelia: change cherrypy from 3.6.0 to 10.2.1
+    # Janelia: change cherrypy from 3.6.0 to 10.2.1 and remove "MySQL-python >= 1.2.5",
     extras_require={
          'device': ['picamera>=1.8', "GitPython >=1.0.1",
-                    "MySQL-python >= 1.2.5", "cherrypy >= 10.2.1", "pyserial>=2.7","bottle>=0.12.8"],
+                     "cherrypy == 10.2.1", "pyserial>=2.7","bottle>=0.12.8"],
          'dev': ['Sphinx >= 1.4.4', "sphinx_rtd_theme >= 0.1.9", "mock >= 2.0.0"]
      },
     setup_requires=[
