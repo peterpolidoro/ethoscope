c82f2c77a34cb01c450ed2e747d3edb48fb81e7a
selmalaki <selmalaki@ucla.edu>
2019-07-12 11:45:54 -0400

    debug content-length header

--- node_src/scripts/server.py
+++ node_src/scripts/server.py
@@ -56,7 +56,7 @@ def enable_cors():
     response.headers['Access-Control-Allow-Methods'] = 'PUT, GET, POST, DELETE, OPTIONS'
     response.headers['Access-Control-Allow-Headers'] = 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token'
     # Janelia
-    print(int(response.headers['Content-Length']))
+    response.headers['Content-Length'] = 50
 
 #################################
 # API to connect with ethoscopes
