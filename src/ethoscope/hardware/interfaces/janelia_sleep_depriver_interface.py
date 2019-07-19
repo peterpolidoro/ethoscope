8fe16810e924ccd7b9c4116e32cf916cc8ef35ec
selmalaki <selmalaki@ucla.edu>
2019-07-19 10:06:32 -0400

    fix comments

--- src/ethoscope/hardware/interfaces/janelia_sleep_depriver_interface.py
+++ src/ethoscope/hardware/interfaces/janelia_sleep_depriver_interface.py
@@ -43,7 +43,7 @@ class JaneliaShakerSleepDepriverInterface(ModularClientInterface):
         :type channel: int
         :param speed: the speed, between -360 and 360 (degree/s) The sign indicates the rotation direction (CW or CCW)
         :type speed: int
-        :param duration: the duration of time to rotate the tube in ms (max is 6s. The masking duration in ethomics is 6s)
+        :param duration: the duration of time to rotate the tube in ms (max is 6s. The masking duration in rethomics tool is 6s)
         :type: duration: int
         :param acc: the acceleration, between 100 and 10000 (degree/sec^2)
         :type: acc: int
@@ -54,7 +54,7 @@ class JaneliaShakerSleepDepriverInterface(ModularClientInterface):
         :param ncycles: the number of oscillation
         :type: ncycles: int
         """
-        print("send: %d %d %d %d" % (speed, duration, acc, dec))
+        print("send shaker: %d %d %d %d" % (speed, duration, acc, dec))
         self.shake_with_speed(board,
                               channel,
                               speed=speed,
