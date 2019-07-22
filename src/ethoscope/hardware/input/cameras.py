45332439ef63e4312b5a37e7a026413ffebea5f0
selmalaki <selmalaki@ucla.edu>
2019-07-22 14:14:35 -0400

    reduce the fps for testing

--- src/ethoscope/hardware/input/cameras.py
+++ src/ethoscope/hardware/input/cameras.py
@@ -403,7 +403,7 @@ class OurPiCameraAsync(BaseCamera):
                                    
 
     _frame_grabber_class = PiFrameGrabber
-    def __init__(self, target_fps=20, target_resolution=(1280, 960), *args, **kwargs):
+    def __init__(self, target_fps=5, target_resolution=(1280, 960), *args, **kwargs):
         """
         Class to acquire frames from the raspberry pi camera asynchronously.
         At the moment, frames are only greyscale images.
