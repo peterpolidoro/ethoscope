7c8673bb13ac29dfeb6642ae424e7f07fe9be764
selmalaki <selmalaki@ucla.edu>
2019-07-22 16:21:34 -0400

    debug the convex hull

--- src/ethoscope/trackers/adaptive_bg_tracker.py
+++ src/ethoscope/trackers/adaptive_bg_tracker.py
@@ -419,7 +419,8 @@ class AdaptiveBGModel(BaseTracker):
             # hulls = [cv2.convexHull( c) for c in contours]
             hulls = contours
             #hulls = merge_blobs(hulls)
-
+            print('contour>1 hulls:')
+            print(hulls)
             hulls = [h for h in hulls if h.shape[0] >= 3]
 
             if len(hulls) < 1:
@@ -436,9 +437,11 @@ class AdaptiveBGModel(BaseTracker):
             distance = all_distances[good_clust]
         else:
             hull = contours[0]
+            print('one hull:')
+            print(hull)
             if hull.shape[0] < 3:
                 self._bg_model.increase_learning_rate()
-                print('hull shape <3 %d' %(hull.shape[0]))
+                print('hull.shape <3 %d' %(hull.shape[0]))
                 raise NoPositionError
 
             features = self.fg_model.compute_features(img, hull)
