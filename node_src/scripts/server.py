c2ab60ae3706aec6497dcc5aca5738449aa5d6b9
selmalaki <selmalaki@ucla.edu>
2019-08-02 11:56:43 -0400

    use cherrypy

--- node_src/scripts/server.py
+++ node_src/scripts/server.py
@@ -353,32 +353,16 @@ if __name__ == '__main__':
         device_scanner = DeviceScanner(LOCAL_IP, results_dir=RESULTS_DIR)
         device_scanner.start()
         #######TO be remove when bottle changes to version 0.13
-        # server = "cherrypy"
-        # try:
-        #     from cherrypy import wsgiserver
-        # except:
-        #     #Trick bottle to think that cheroot is actulay cherrypy server adds the pacth to BOTTLE
-        #     server_names["cherrypy"]=CherootServer(host='0.0.0.0', port=PORT)
-        #     logging.warning("Cherrypy version is bigger than 9, we have to change to cheroot server")
-        #     pass
-        # #########
-        # run(app, host='0.0.0.0', port=PORT, debug=DEBUG, server='cherrypy')
-
-        SERVER = "cheroot"
-        #######To be remove when bottle changes to version 0.13
+        server = "cherrypy"
         try:
-            #This checks if the patch has to be applied or not. We check if bottle has declared cherootserver
-            #we assume that we are using cherrypy > 9
-            from bottle import CherootServer
+            from cherrypy import wsgiserver
         except:
-            #Trick bottle to think that cheroot is actulay cherrypy server, modifies the server_names allowed in bottle
-            #so we use cheroot in background.
-            SERVER="cherrypy"
+            #Trick bottle to think that cheroot is actulay cherrypy server adds the pacth to BOTTLE
             server_names["cherrypy"]=CherootServer(host='0.0.0.0', port=PORT)
             logging.warning("Cherrypy version is bigger than 9, we have to change to cheroot server")
             pass
         #########
-        run(app, host='0.0.0.0', port=PORT, debug=DEBUG, server=SERVER)
+        run(app, host='0.0.0.0', port=PORT, debug=DEBUG, server='cherrypy')
 
     except KeyboardInterrupt:
         logging.info("Stopping server cleanly")
